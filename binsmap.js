55fe39f585e69ff2a612219d1dcbfd09a0504f4d
Steve Bennett <stevage@gmail.com>
2017-07-29 14:51:57 +1000

    More http:// --> //

--- binsmap.js
+++ binsmap.js
@@ -310,18 +310,18 @@ $(function() {
     
     attribution = 'Steve Bennett + Geelong, Wyndham, Golden Plains, Ballarat, Manningham councils';
     var updated = '11';
-    tiles['Rubbish'] = L.tileLayer('http://guru.cycletour.org/tile/openbins-rubbish/{z}/{x}/{y}.png?updated=' + updated, { 
+    tiles['Rubbish'] = L.tileLayer('//guru.cycletour.org/tile/openbins-rubbish/{z}/{x}/{y}.png?updated=' + updated, { 
         maxZoom: 18, attribution: attribution });
-    tiles['Recycling'] = L.tileLayer('http://guru.cycletour.org/tile/openbins-recycling/{z}/{x}/{y}.png?updated=' + updated, { 
+    tiles['Recycling'] = L.tileLayer('//guru.cycletour.org/tile/openbins-recycling/{z}/{x}/{y}.png?updated=' + updated, { 
         maxZoom: 18, attribution: attribution });
-    tiles['Green waste'] = L.tileLayer('http://guru.cycletour.org/tile/openbins-green/{z}/{x}/{y}.png?updated=' + updated, { 
+    tiles['Green waste'] = L.tileLayer('//guru.cycletour.org/tile/openbins-green/{z}/{x}/{y}.png?updated=' + updated, { 
         maxZoom: 18, attribution: attribution });
-    tiles['Positron'] = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+    tiles['Positron'] = L.tileLayer('//{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
         attribution: 'CartoDB, OpenStreetMap'});
-    overlays['Suburbs'] = L.tileLayer('http://guru.cycletour.org/tile/Suburbs/{z}/{x}/{y}.png?updated=1', {
+    overlays['Suburbs'] = L.tileLayer('//guru.cycletour.org/tile/Suburbs/{z}/{x}/{y}.png?updated=1', {
         attribution: 'Steve Bennett, OpenStreetMap'});
-    overlays['Tips and landfills'] = L.tileLayer('http://guru.cycletour.org/tile/openbinmap-national-db/{z}/{x}/{y}.png');
-    overlays['Coverage'] = L.tileLayer('http://guru.cycletour.org/tile/openbins-nocoverage/{z}/{x}/{y}.png?updated=' + updated, {
+    overlays['Tips and landfills'] = L.tileLayer('//guru.cycletour.org/tile/openbinmap-national-db/{z}/{x}/{y}.png');
+    overlays['Coverage'] = L.tileLayer('//guru.cycletour.org/tile/openbins-nocoverage/{z}/{x}/{y}.png?updated=' + updated, {
      opacity: 0.5 });
     map = L.map('map', {layers: [tiles.Positron]}).setView([-37.7, 144.5], 8);
 
