1bcaf194b2349696fa5becb7e08b35080c762bca
Matthew Cengia <mattcen@gmail.com>
2018-08-22 13:00:47 +1000

    Update dumpbins

--- 4-dumpbins.sh
+++ 4-dumpbins.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+DBNAME=bins
+TABLE=allbins
+
 mkdir export 2>/dev/null
 rm -f export/*.topojson
 rm -f export/*.geojson
 
-ogr2ogr -f GeoJSON export/allbins.geojson -t_srs EPSG:4326  "PG:host=localhost dbname=bins user=ubuntu password=ubuntu" -sql "select * from allbins;"
+ogr2ogr -f GeoJSON export/allbins.geojson -t_srs EPSG:4326  "PG:dbname=$DBNAME" -sql "select * from $TABLE;"
 topojson export/allbins.geojson --properties -o export/allbins.topojson
