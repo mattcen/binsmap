cb986e4cae8529353f0e6a86791eaaf17738a576
Matthew Cengia <mattcen@gmail.com>
2018-08-22 12:49:30 +1000

    Update mergebins

--- 3-mergebins.sh
+++ 3-mergebins.sh
@@ -1,4 +1,16 @@
+#!/bin/bash
 DBNAME=bins
+TABLE=allbins
+function showcount() {
+  t=$TABLE
+  total=" in total."
+  if [[ -n "$1" ]]; then
+    t=$1
+    total=""
+  fi
+  psql -d $DBNAME -c "select concat(count(*), ' $TABLE $total') from $t;" | grep "$TABLE"
+}
+
 psql -d $DBNAME < 3a-mergebins.sql
 showcount 
 psql -d $DBNAME < 3b-cleanbins.sql
